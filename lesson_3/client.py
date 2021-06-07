d69d5cff112b0c9bbcefbfe17f2fb748469c2959
Salamandr <serega_17_94@mail.ru>
2021-06-07 17:53:24 +0300

    lesson_3

---
+++ lesson_3/client.py
@@ -0,0 +1,29 @@
+from socket import socket, AF_INET, SOCK_STREAM
+import pickle
+import time
+
+s = socket(AF_INET, SOCK_STREAM)
+
+
+def init_socket():
+    s.connect(('localhost', 8777))
+
+
+def main():
+    msg = {
+        "action": "authenticate",
+        "time": time.time(),
+        "user": {
+            "account_name": "C0deMaver1ck",
+            "password": "CorrectHorseBatteryStaple"
+        }
+    }
+    s.send(pickle.dumps(msg))
+    data = s.recv(1024)
+    print('Сообщение от сервера:', pickle.loads(data), 'длинной ', len(data)),
+    s.close()
+
+
+if __name__ == '__main__':
+    init_socket()
+    main()
